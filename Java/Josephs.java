6cff319e2740864908df6868d9ff8fd159dec395
최정균 <wjdrbs966@naver.com>
2020-01-03 22:34:39 +0900

    디렉토리 정리중

---
+++ Java/Josephs.java
@@ -0,0 +1,26 @@
+//백준 1158번 문제
+package Algorithm;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Josephs {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            list.add(i + 1);
+        }
+        int c = b - 1;
+        while (list.size() > 1) {
+            System.out.print(list.remove(c));
+            c = c + 2;
+            c = c % list.size();
+        }
+        System.out.println(list.remove()+">");
+    }
+}
