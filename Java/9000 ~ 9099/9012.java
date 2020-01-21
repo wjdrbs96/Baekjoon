c83a1ba638e82717e574fd7178480e17024e87ee
최정균 <wjdrbs966@naver.com>
2020-01-21 21:14:37 +0900

    백준 괄호문제

---
+++ Java/9000 ~ 9099/9012.java
@@ -0,0 +1,50 @@
+package Java_Algorithm;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+// 어떻게 풀어야 할지는 되게 쉬웠는데 YES NO 를 출력하는 부분의 boolean 을 사용하는 팁을 얻은 구글링 한 문제
+
+public class Main_9012 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        input.nextLine();
+        for (int i = 0; i < n; ++i) {
+            Stack<Character> stack = new Stack();
+            boolean b = true;
+            String s = input.nextLine();
+
+            for (int j = 0; j < s.length(); ++j) {
+                if (s.charAt(j) == '(') {
+                    stack.push(s.charAt(j));
+                }
+
+                else if (s.charAt(j) == ')') {
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    }
+
+                    else {
+                        b = false;
+                        break;
+                    }
+                }
+            }
+
+            if(!stack.isEmpty()) {
+                b = false;
+            }
+
+            if(b) {
+                System.out.println("YES");
+            }
+
+            else {
+                System.out.println("NO");
+            }
+
+        }
+    }
+}
--- Python/11600 ~ 11699/11650.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-l = []
-
-for i in range(n):
-    l.append((map(int, input().split())))
-
