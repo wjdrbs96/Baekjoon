20ecb9d16d784fff283f52178ad3bef01290f6f9
최정균 <wjdrbs966@naver.com>
2020-01-22 22:35:14 +0900

    1149번 백준

---
+++ Java/1100 ~ 1199/1149.java
@@ -0,0 +1,27 @@
+package Java_Algorithm;
+
+import java.util.Scanner;
+
+public class Main_1149 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[][] list = new int[n + 1][3];
+
+        for (int i = 1; i <= n ; ++i) {
+            list[i][0] = input.nextInt();
+            list[i][1] = input.nextInt();
+            list[i][2] = input.nextInt();
+        }
+
+        for(int i = 2; i <= n; ++i) {
+            list[i][0] += Math.min(list[i-1][1], list[i-1][2]);
+            list[i][1] += Math.min(list[i-1][0], list[i-1][2]);
+            list[i][2] += Math.min(list[i-1][0], list[i-1][1]);
+        }
+
+        System.out.println(Math.min(list[n][0], Math.min(list[n][1], list[n][2])));
+
+    }
+}
