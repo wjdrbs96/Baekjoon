7e19765e879bd16245e4e33e617dc1673ab0d030
최정균 <wjdrbs966@naver.com>
2020-01-25 00:01:33 +0900

    백준 문제 solved easy

---
+++ Java/9400 ~ 9499/9498.java
@@ -0,0 +1,27 @@
+package Java_Algorithm;
+
+import java.util.Scanner;
+
+public class Main_9498 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        if (n >= 90 && n <= 100) {
+            System.out.println("A");
+        }
+        else if (n >=80 && n < 90) {
+            System.out.println("B");
+        }
+        else if (n >=70 && n < 80) {
+            System.out.println("C");
+        }
+        else if (n >=60 && n < 70) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
+        }
+    }
+}
