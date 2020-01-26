dc8c34ea17b0fb62f997ea15e5c35b52b7cea8d8
최정균 <wjdrbs966@naver.com>
2020-01-27 03:58:49 +0900

    숨바꼭질 문제가 BFS 일줄이야..

---
+++ Java/1600 ~ 1699/1697.java
@@ -0,0 +1,50 @@
+package Java_Algorithm;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main_1697 {
+    static int N, K;
+    static int[] visit = new int[100001];
+
+    public static void BFS() {
+        Queue<Integer> q = new LinkedList<>();
+
+        q.offer(N);
+        visit[N] = 1;
+
+        while(!q.isEmpty()) {
+            N = q.poll();
+
+            if (N == K) break;
+
+            if (N + 1 <= 100000 && visit[N+1] == 0) {
+                q.offer(N + 1);
+                visit[N + 1] = visit[N] + 1;
+            }
+
+            if (N - 1 >= 0 && visit[N-1] == 0) {
+                q.offer(N - 1);
+                visit[N - 1] = visit[N] + 1;
+            }
+
+            if (N * 2 <= 100000 && visit[N*2] == 0) {
+                q.offer(N * 2);
+                visit[N * 2] = visit[N] + 1;
+            }
+        }
+        System.out.println(visit[K] - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        N = input.nextInt();
+        K = input.nextInt();
+
+        BFS();
+
+
+    }
+}
