2c641c187f8027329cd4b44af5f2bb938efa56f7
최정균 <wjdrbs966@naver.com>
2020-01-28 00:44:26 +0900

    백준 별찍기 2442번

---
+++ Java/2400 ~ 2499/2442.java
@@ -0,0 +1,21 @@
+package Java_Algorithm;
+
+import java.util.Scanner;
+
+public class Main_2442 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        final int N = sc.nextInt();
+
+        for(int i = 1; i <= N ; i++) {
+            for(int j = 0; j < N-i; j++)
+                System.out.print(" ");
+            for(int j = 0; j < i*2-1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+    }
+}
