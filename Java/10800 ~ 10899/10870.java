1390e3907f23420fd8fe7355b0baf525db842857
최정균 <wjdrbs966@naver.com>
2020-02-02 00:40:25 +0900

    BOJ Recursive fibonacci Solved

---
+++ Java/10800 ~ 10899/10870.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Main_10870 {
+
+    public static int fibo(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        System.out.println(fibo(n));
+    }
+}
