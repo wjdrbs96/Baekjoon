685ef82096b61ba1c6a35b0f030a6f1e0c2ae153
최정균 <wjdrbs966@naver.com>
2020-02-03 18:56:07 +0900

    백준 미로탐색 BFS 문제 아직은 좀 어렵긴 하다

---
+++ Java/BOJ2100/Main_2178.java
@@ -0,0 +1,74 @@
+package Baekjoon.Java.BOJ2100;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Save {
+    int x;
+    int y;
+
+    public Save(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main_2178 {
+    static int N;
+    static int M;
+    static int[][] adj;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static boolean[][] visited;
+
+
+    public static void BFS(int x, int y) {
+        Queue<Save> q = new LinkedList<Save>();
+        q.add(new Save(x, y));
+
+        while (!q.isEmpty()) {
+            Save s = q.poll();
+
+            for (int i = 0; i < 4; ++i) {
+                int nx = s.x + dx[i];
+                int ny = s.y + dy[i];
+
+                if (nx >= 1 && nx <= N && ny >= 1 && ny <= M) {
+                    if (adj[nx][ny] == 1 && !visited[nx][ny]) {
+                        q.add(new Save(nx, ny));
+                        adj[nx][ny] = adj[s.x][s.y] + 1;
+                        visited[nx][ny] = true;
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        N = input.nextInt();
+        M = input.nextInt();
+        adj = new int[N + 1][M + 1];
+        visited = new boolean[N + 1][M + 1];
+
+        for (int i = 1; i <= N; ++i) {
+            String s = input.next();
+            for (int j = 1; j <= M; ++j) {
+                adj[i][j] = s.charAt(j - 1) - '0';
+            }
+        }
+
+        BFS(1, 1);
+        System.out.println(adj[N][M]);
+
+
+
+
+
+
+    }
+}
