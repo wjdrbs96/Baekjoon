0a3c5e32e741fc170e99309b28ec1b6c93594eff
최정균 <wjdrbs966@naver.com>
2020-02-05 17:54:25 +0900

    백준 연속합 동적 계획법 수정

--- Java/BOJ1900/Main_1912.java
+++ Java/BOJ1900/Main_1912.java
@@ -9,13 +9,13 @@ public class Main_1912 {
         int n = input.nextInt();
         int[] dp = new int[n];       // 더한 것을 저장하는 배열
         int[] ary = new int[n];      // 입력 받은 것을 저장
-        int max = 0;
 
         for (int i = 0; i < n; ++i) {
             ary[i] = input.nextInt();
         }
 
         dp[0] = ary[0];
+        int max = ary[0];
         for (int i = 1; i < n; ++i) {
             dp[i] = Math.max(dp[i - 1] + ary[i], ary[i]);   // 2개 더한 것과 다음 것의 max
             max = Math.max(max, dp[i]);
