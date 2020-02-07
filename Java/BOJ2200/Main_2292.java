d2d04a14240a915fb89f42b8abf2007a197c9119
최정균 <wjdrbs966@naver.com>
2020-02-08 00:46:17 +0900

    백준 벌집 문제 어렵네..

--- Java/BOJ2200/Main_2292.java
+++ Java/BOJ2200/Main_2292.java
@@ -5,7 +5,22 @@ import java.util.Scanner;
 public class Main_2292 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
 
-        
+        int result = 1;
+        int count = 1;
+        int addNum = 6;
+
+        while(true) {
+            if (n <= count) {
+                break;
+            }
+
+            count += addNum;
+            addNum += 6;
+            result++;
+        }
+
+        System.out.println(result);
     }
 }
