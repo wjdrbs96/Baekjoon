9802772c5f61cf4c0bcb0a601910b97dd9000956
최정균 <wjdrbs966@naver.com>
2020-02-20 01:45:01 +0900

    어렵다

---
+++ Java/BOJ3000/Main_3009.java
@@ -0,0 +1,42 @@
+package Baekjoon.Java.BOJ3000;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_3009 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] listX = new int[3];
+        int[] listY = new int[3];
+
+        for (int i = 0; i < 3; ++i) {
+            listX[i] = input.nextInt();
+            listY[i] = input.nextInt();
+        }
+
+        Arrays.sort(listX);
+        Arrays.sort(listY);
+
+        int saveX = 0;
+        if (listX[0] != listX[1]) {
+            saveX = listX[0];
+        } else {
+            saveX = listX[2];
+        }
+
+        int saveY = 0;
+        if (listY[0] != listY[1]) {
+            saveY = listY[0];
+        } else {
+            saveY = listY[2];
+        }
+
+        System.out.println(saveX + " " + saveY);
+
+
+
+
+
+    }
+}
