9a06269439253bdab7ccddd539ce17ff7e670dcb
최정균 <wjdrbs966@naver.com>
2020-02-21 20:17:48 +0900

    좀 오래걸린 스택활용문제

---
+++ Java/BOJ17400/Main_17413.java
@@ -0,0 +1,54 @@
+package Baekjoon.Java.BOJ17400;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main_17413 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean visit = false;
+        Stack<Character> stack = new Stack<>();
+
+        String name = input.nextLine();
+
+
+        for (int i = 0; i < name.length(); ++i) {
+            if (name.charAt(i) == ' ') {
+                while (!stack.isEmpty()) {
+                    System.out.print(stack.pop());
+                }
+                System.out.print(" ");
+            }
+            else if (name.charAt(i) == '<') {
+                visit = true;
+                while (!stack.isEmpty()) {
+                    System.out.print(stack.pop());
+                }
+
+                System.out.print('<');
+            }
+            else if (name.charAt(i) == '>') {
+                StringBuilder sb = new StringBuilder();
+                while (!stack.isEmpty()) {
+                    sb.append(stack.pop());
+                }
+                sb.reverse();
+                System.out.print(sb.toString());
+                System.out.print('>');
+                visit = false;
+            }
+            else {
+                if (visit == false) {
+                    stack.push(name.charAt(i));
+                } else {
+                    System.out.print(name.charAt(i));
+                }
+
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+    }
+}
