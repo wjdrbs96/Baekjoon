a7e669ed082569ff7b1fba41b7dc4b0bf70b36ae
최정균 <wjdrbs966@naver.com>
2020-02-29 00:54:58 +0900

    아 문제는 쉬운데 조건이 좀 까다로웠군

---
+++ Java/BOJ10800/Main_10816.java
@@ -0,0 +1,25 @@
+package Baekjoon.Java.BOJ10800;
+
+import java.util.Scanner;
+
+public class Main_10816 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        // 입력 값에 음수가 입기 때문에 배열의 크기를 2배로 늘려서 함
+        int[] card = new int[20000001];
+        for (int i = 0; i < N; ++i) {
+            ++card[sc.nextInt()+10000000] ;
+        }
+        int M = sc.nextInt();
+
+        for (int i = 0; i < M; ++i) {
+            sb.append(card[sc.nextInt()+10000000]+" ");
+        }
+        System.out.println(sb.toString());
+    }
+
+}
