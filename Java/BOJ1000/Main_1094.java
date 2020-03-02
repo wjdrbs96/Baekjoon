6c46a0e251e34c096813199b14aa0639a383bf50
최정균 <wjdrbs966@naver.com>
2020-03-02 23:07:28 +0900

    막대기 문제

---
+++ Java/BOJ1000/Main_1094.java
@@ -0,0 +1,35 @@
+package Baekjoon.Java.BOJ1000;
+
+
+import java.util.Scanner;
+
+public class Main_1094 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int result = 0;
+        int len = 64;
+        int cnt = 0;
+        int end = num;
+        while(true) {
+            if(num == 64) {
+                cnt++;
+                break;
+            }
+            len = len / 2;
+
+            if(num >= len) {
+                result += len;
+                cnt++;
+                if(end == result)
+                {
+                    break;
+                }
+                num = num - len;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+}
