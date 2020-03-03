02c875fdcb59aa5de3def6fe922db4b88e034826
최정균 <wjdrbs966@naver.com>
2020-03-03 23:44:25 +0900

    백준 마이크로소프트

---
+++ Java/BOJ5300/Main_5338.java
@@ -0,0 +1,12 @@
+package Baekjoon.Java.BOJ5300;
+
+public class Main_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+
+}
