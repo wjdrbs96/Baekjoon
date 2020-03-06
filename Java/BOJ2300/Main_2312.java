53041a3d28f23b1b28d360070acc6225ef8ac541
최정균 <wjdrbs966@naver.com>
2020-03-06 12:46:37 +0900

    백준 수 복원하기 solved

--- Java/BOJ2300/Main_2312.java
+++ Java/BOJ2300/Main_2312.java
@@ -3,19 +3,37 @@ package Baekjoon.Java.BOJ2300;
 import java.util.Scanner;
 
 public class Main_2312 {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         int n = input.nextInt();
 
         for (int i = 0; i < n; ++i) {
+            int[] list = new int[100001];
             int test = input.nextInt();
 
-            int j = 2;
-            int count = 0;
-            while (test > 0) {
-
+            for (int j = 2; j * j <= test; ++j) {
+                if (test % j == 0) {
+                    test = test / j;
+                    list[j]++;
+                    j--;
+                }
             }
+
+            if (test > 1) {
+                list[test]++;
+            }
+
+            for (int j = 1; j <= 100000; ++j) {
+                if (list[j] != 0) {
+                    System.out.println(j +" " + list[j]);
+                }
+            }
+
+
         }
     }
+
+
 }
