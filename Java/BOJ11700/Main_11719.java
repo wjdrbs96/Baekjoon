7ebe1034ca8b37ba547ab64ac932066a9f568d17
최정균 <wjdrbs966@naver.com>
2020-03-10 03:00:44 +0900

    그대로 출력하기2 11719 solved

---
+++ Java/BOJ11700/Main_11719.java
@@ -0,0 +1,14 @@
+package Baekjoon.Java.BOJ11700;
+
+import java.util.Scanner;
+
+public class Main_11719 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNextLine()) {
+            String s = input.nextLine();
+            System.out.println(s);
+        }
+    }
+}
