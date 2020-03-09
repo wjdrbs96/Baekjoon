59cd0066e1171f99ec3a983b6ea1e1e7cf2e62f5
최정균 <wjdrbs966@naver.com>
2020-03-10 02:18:54 +0900

    열개씩 끊어서 출력하기 자바로 다시 풀기

---
+++ Java/BOJ11700/Main_11721.java
@@ -0,0 +1,26 @@
+package Baekjoon.Java.BOJ11700;
+
+import java.util.Scanner;
+
+public class Main_11721 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String s = input.next();
+
+        int len = s.length() / 10;
+
+        int i = 0;
+        while (len-- > 0) {
+            System.out.println(s.substring(i, i + 10));
+            i += 10;
+        }
+
+        if (s.length() % 10 > 0) {
+            System.out.println(s.substring(i));
+        }
+
+
+
+    }
+}
