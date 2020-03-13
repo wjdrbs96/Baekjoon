37fb03d66273159f5edbab2aad974f2637538ed7
최정균 <wjdrbs966@naver.com>
2020-03-13 11:18:20 +0900

    백준 BigInteger 소음 2935 solved

---
+++ Java/BOJ2900/Main_2935.java
@@ -0,0 +1,21 @@
+package Baekjoon.Java.BOJ2900;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main_2935 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        BigInteger A = new BigInteger(input.next());
+        String ch = input.next();
+        BigInteger B = new BigInteger(input.next());
+
+        if (ch.equals("+")) {
+            System.out.println(A.add(B));
+        }
+        else if (ch.equals("*")) {
+            System.out.println(A.multiply(B));
+        }
+    }
+}
