19bf071d34c086609a6ae3f81e1c2222c319d593
최정균 <wjdrbs966@naver.com>
2020-03-15 01:53:04 +0900

    README.md 수정중

--- Java/BOJ11000/Main_11025.java
+++ Java/BOJ11000/Main_11025.java
@@ -1,32 +1,30 @@
-package Baekjoon.Java.BOJ11800;
+package Baekjoon.Java.BOJ11000;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main_11866 {
+public class Main_11025 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
+
         int N = input.nextInt();
         int K = input.nextInt() - 1;
         int idx = K;
 
-        List<Integer> list = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < N; ++i) {
-            list.add(i + 1);
+            list.add(i);
         }
-        sb.append("<");
+
 
         while (list.size() != 1) {
-            sb.append(list.get(idx).toString() + ", ");
             list.remove(idx);
             idx = (idx + K) % list.size();
         }
 
-        sb.append(list.get(0).toString() + ">");
-        System.out.println(sb);
+
 
 
     }
