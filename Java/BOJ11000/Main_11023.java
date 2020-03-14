d9d32e2deff446f2e4927ba183cc9afbc42e1334
최정균 <wjdrbs966@naver.com>
2020-03-15 00:32:49 +0900

    백준 더하기 3 11023 solved

---
+++ Java/BOJ11000/Main_11023.java
@@ -0,0 +1,21 @@
+package Baekjoon.Java.BOJ11000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11023 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+
+        while (st.hasMoreTokens()) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(sum);
+    }
+}
