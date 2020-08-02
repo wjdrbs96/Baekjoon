a58c51c3e18b147fbd9ca0e3011aa6a43c8707ae
최정균 <wjdrbs966@naver.com>
2020-08-02 20:37:43 +0900

    Baekjoon Greedy 5585 with Java

---
+++ Java/BOJ5500/Main_5585.java
@@ -0,0 +1,25 @@
+package Baekjoon.Java.BOJ5500;
+
+import java.util.Scanner;
+
+public class Main_5585 {
+    public static void main(String [] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int change = 1000 - scanner.nextInt();
+        int result = 0;
+
+        int [] coins = {500,100,50,10,5,1};
+
+        for(int coin : coins){
+
+            if (coin > change)
+                continue;
+            result += change / coin;
+            change %= coin;
+
+        }
+        System.out.println(result);
+    }
+}
