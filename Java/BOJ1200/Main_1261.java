7eb0c9c2d9493ed609626a2c2841799c55574bef
최정균 <wjdrbs966@naver.com>
2020-10-01 17:21:53 +0900

    bfs/dfs 알고스팟 add

--- Java/BOJ1200/Main_1261.java
+++ Java/BOJ1200/Main_1261.java
@@ -1,14 +1,14 @@
 package Baekjoon.Java.BOJ1200;
 
-
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main_1261 {
     static int N;
     static int M;
     static int[][] adj;
+    static int[][] dist;
     static boolean[][] visit;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
@@ -20,45 +20,52 @@ public class Main_1261 {
         N = input.nextInt();
 
         adj = new int[N + 1][M + 1];
+        dist = new int[N + 1][M + 1];
         visit = new boolean[N + 1][M + 1];
 
-        int j = 1;
         for (int i = 1; i <= N; ++i) {
+            int j = 1;
             String line = input.next();
             for (char ch : line.toCharArray()) {
                 adj[i][j] = Integer.parseInt(String.valueOf(ch));
+                j++;
             }
-            j++;
         }
 
         bfs(new Miro(1, 1));
-        System.out.println(adj[M][N]);
+
     }
 
-    static void bfs(Miro miro) {
-        Queue<Miro> queue = new LinkedList<>();
-        queue.offer(miro);
 
-        while (!queue.isEmpty()) {
-            Miro miro1 = queue.poll();
+    static void bfs(Miro miro) {
+        Deque<Miro> deque = new LinkedList<>();
+        deque.addLast(miro);
+        visit[miro.x][miro.y] = true;
+
+        while (!deque.isEmpty()) {
+            Miro miro1 = deque.pollLast();
             int x = miro1.x;
             int y = miro1.y;
-
 
             for (int i = 0; i < 4; ++i) {
                 int nx = dx[i] + x;
                 int ny = dy[i] + y;
 
-                if (nx > 0 && nx < N + 1 && ny > 0 && ny < M + 1) {
-                    if (!visit[nx][ny]) {
-                        adj[nx][ny] += adj[x][y];
-                        visit[nx][ny] = true;
-                        queue.offer(new Miro(nx, ny));
+                if (nx > 0 && nx < N + 1 && ny > 0 && ny < M + 1 && !visit[nx][ny]) {
+                    if (adj[nx][ny] == 1) {
+                        dist[nx][ny] = dist[x][y] + 1;
+                        deque.addFirst(new Miro(nx, ny));
                     }
+                    else {
+                        dist[nx][ny] = dist[x][y];
+                        deque.addLast(new Miro(nx, ny));
+                    }
+                    visit[nx][ny] = true;
                 }
             }
-
         }
+
+        System.out.println(dist[N][M]);
     }
 }
 
