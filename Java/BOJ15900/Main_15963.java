a5f530b66b821927f2d9ef9b37a0172e2e578a94
최정균 <wjdrbs966@naver.com>
2020-10-03 19:11:59 +0900

    15963 CASIO add

---
+++ Java/BOJ15900/Main_15963.java
@@ -0,0 +1,19 @@
+package Baekjoon.Java.BOJ15900;
+
+import java.util.Scanner;
+
+public class Main_15963 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String N = input.next();
+        String M = input.next();
+
+        if (N.equals(M)) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
