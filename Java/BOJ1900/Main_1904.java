f2c21a63d28e2237472257a56ecd167561ee4e91
최정균 <wjdrbs966@naver.com>
2020-10-04 14:29:25 +0900

    백준 DP 1904 01 타일 add

--- Java/BOJ1900/Main_1904.java
+++ Java/BOJ1900/Main_1904.java
@@ -7,15 +7,13 @@ public class Main_1904 {
         Scanner input = new Scanner(System.in);
 
         int n = input.nextInt();
-        int[] list = new int[n + 1];
+        int[] list = new int[1100000];
 
         list[1] = 1; list[2] = 2;
-        for (int i = 3; i <= n; ++i) {
-            list[i] = (list[i-1] + list[i-2]) % 15746;
+        for (int i = 3; i < 1100000; ++i) {
+            list[i] = (list[i - 1] + list[i - 2]) % 15746;
         }
 
         System.out.println(list[n]);
-
-
     }
 }
