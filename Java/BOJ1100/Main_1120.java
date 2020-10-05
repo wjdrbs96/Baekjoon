f1e7f5424522866acd5093c5341e7fa4a20607d4
최정균 <wjdrbs966@naver.com>
2020-10-05 19:58:04 +0900

    백준 brute force 문자열 add

---
+++ Java/BOJ1100/Main_1120.java
@@ -0,0 +1,36 @@
+package Baekjoon.Java.BOJ1100;
+
+import java.util.*;
+
+public class Main_1120 {
+    static List<Integer> listCount = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String ab = input.nextLine();
+
+        String[] list = ab.split(" ");
+
+        for (int i = 0; i < list[0].length(); ++i) {
+            for (int j = 0; j < list[1].length() - list[0].length() + 1; ++j) {
+                stringDiffCount(list[0], list[1].substring(j, j + list[0].length()));
+            }
+        }
+
+        System.out.println(Collections.min(listCount));
+    }
+
+    static void stringDiffCount(String a, String b) {
+        if (a.length() != b.length()) {
+            return;
+        }
+
+        int count = 0;
+        for (int i = 0; i < a.length(); ++i) {
+            if (a.charAt(i) != b.charAt(i)) {
+                count++;
+            }
+        }
+        listCount.add(count);
+    }
+}
