201b70865d1c915cd5479e80a378f6429766521a
Gyunny <wjdrbs966@naver.com>
2020-10-25 17:01:32 +0900

    BOJ 1920 add

---
+++ Java/BOJ1900/Main_1920_2.java
@@ -0,0 +1,28 @@
+package Baekjoon.Java.BOJ1900;
+
+import java.util.*;
+
+public class Main_1920_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < N; ++i) {
+            map.put(input.nextInt(), 1);
+        }
+
+        int M = input.nextInt();
+
+        for (int i = 0; i < M; ++i) {
+            int num = input.nextInt();
+            if (map.get(num) != null) {
+                System.out.println(1);
+            }
+            else {
+                System.out.println(0);
+            }
+        }
+    }
+}
