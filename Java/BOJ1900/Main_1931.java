cf5e7648ac8feefaf2f6d7673cd60fcf73558126
Gyunny <wjdrbs966@naver.com>
2020-11-19 23:33:55 +0900

    BOJ 회의실 배정 1931

--- Java/BOJ1900/Main_1931.java
+++ Java/BOJ1900/Main_1931.java
@@ -1,5 +1,8 @@
 package Baekjoon.Java.BOJ1900;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main_1931 {
@@ -7,12 +10,43 @@ public class Main_1931 {
         Scanner input = new Scanner(System.in);
 
         int N = input.nextInt();
+        List<Session> listSession = new ArrayList<>();
 
         for (int i = 0; i < N; ++i) {
             int a = input.nextInt();
             int b = input.nextInt();
 
-
+            listSession.add(new Session(a, b));
         }
+
+        // 회의 종료 시간 정렬 (종료 시간이 빠르면 더 많은 활동을 할 수 있다)
+        Collections.sort(listSession, ((o1, o2) -> {
+            if (o1.endTime == o2.endTime) return o1.startTime - o2.startTime;
+            return o1.endTime - o2.endTime;
+        }));
+
+        Session session1 = listSession.get(0);
+        int count = 1;
+
+        for (int i = 1; i < listSession.size(); ++i) {
+            Session session2 = listSession.get(i);
+
+            if (session1.endTime <= session2.startTime) {
+                count++;
+                session1 = session2;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
+
+class Session {
+    int startTime;
+    int endTime;
+
+    public Session(int startTime, int endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 }
