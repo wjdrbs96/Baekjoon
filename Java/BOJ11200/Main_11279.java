22847f165a8a438e7e8779ac22f97f6ac5dbf30e
Gyunny <wjdrbs966@naver.com>
2020-11-27 09:58:22 +0900

    BOJ 11279 solve

---
+++ Java/BOJ11200/Main_11279.java
@@ -0,0 +1,30 @@
+package Baekjoon.Java.BOJ11200;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main_11279 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt(); // 연산의 개수
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; ++i) {
+            int a = input.nextInt();
+
+            if (a == 0) {
+                if (!priorityQueue.isEmpty()) {
+                    System.out.println(priorityQueue.poll());
+                }
+                else {
+                    System.out.println(0);
+                }
+            }
+            else {
+                priorityQueue.offer(a);
+            }
+        }
+    }
+}
