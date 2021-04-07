ec86f93bea530705b4a49e8a8964676691443289
wjdrbs96 <wjdrbs966@naver.com>
2021-04-08 00:13:00 +0900

    BOJ 2212 add

--- Java/BOJ2200/Main_2212.java
+++ Java/BOJ2200/Main_2212.java
@@ -1,27 +1,41 @@
 package Baekjoon.Java.BOJ2200;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main_2212 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = input.nextInt();  // 센서 수
-        int K = input.nextInt();  // 기지국 수
-        int[] list = new int[N + 1];
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i < N + 1; ++i) {
-            list[i] = input.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; ++i) {
+            list[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(list);
 
-        int countK = 0; // 가능한 기지국 수 카운트
+        Integer[] diff = new Integer[N - 1];
 
-        for (int i = 1; i < list.length; ++i) {
-
+        for (int i = 0; i < N - 1; ++i) {
+            diff[i] = list[i + 1] - list[i];
         }
 
+        Arrays.sort(diff, Collections.reverseOrder());
+
+        int sum = 0;
+        for (int i = K - 1; i < N - 1; ++i) {
+            sum += diff[i];
+        }
+
+        System.out.println(sum);
     }
 }
