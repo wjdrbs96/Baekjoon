fe3884df59252728652c94639c00d64e4cfdb0eb
wjdrbs96 <wjdrbs966@naver.com>
2021-04-13 23:50:32 +0900

    BOJ 15651 add

--- Java/BOJ15600/Main_15651.java
+++ Java/BOJ15600/Main_15651.java
@@ -1,23 +1,37 @@
 package Baekjoon.Java.BOJ15600;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main_15651 {
-    static int n, m;
-    static boolean[] check;
+    private static int N, M;
+    private static StringBuilder sb = new StringBuilder();
+    private static StringBuilder sb1 = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = input.nextInt();
-        m = input.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        dfs();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        combination(0);
+        System.out.println(sb1);
     }
 
-    static void dfs() {
-        for (int i = 0; i < n; ++i) {
+    private static void combination(int depth) {
+        if (depth == M) {
+            sb1.append(sb).append("\n");
+            return;
+        }
 
+        for (int i = 1; i <= N; ++i) {
+            sb.append(i).append(" ");
+            combination(depth + 1);
+            sb.delete(sb.length() - 2, sb.length());
         }
     }
 }
