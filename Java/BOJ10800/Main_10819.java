1cd3ee1a15a64f5e5964b8e38c1cd6de1306c2d3
wjdrbs96 <wjdrbs966@naver.com>
2021-04-16 13:39:11 +0900

    BOJ 10819 add

--- Java/BOJ10800/Main_10819.java
+++ Java/BOJ10800/Main_10819.java
@@ -1,18 +1,53 @@
 package Baekjoon.Java.BOJ10800;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main_10819 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    private static int N;
+    private static int[] list;
+    private static int[] list2;
+    private static boolean[] visit;
+    private static int sum = 0;
+    private static int max = Integer.MIN_VALUE;
 
-        int N = input.nextInt();
-        int[] list = new int[N + 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        N = Integer.parseInt(br.readLine());
+
+        list = new int[N];
+        list2 = new int[N];
+        visit = new boolean[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; ++i) {
-            list[i] = input.nextInt();
+            list[i] = Integer.parseInt(st.nextToken());
         }
 
+        backTracking(0);
+        System.out.println(max);
+    }
 
+    private static void backTracking(int depth) {
+        if (depth == N) {
+            for (int i = 0; i < list.length - 1; ++i) {
+                sum += Math.abs(list2[i + 1] - list2[i]);
+            }
+
+            max = Math.max(max, sum);
+            sum = 0;
+            return;
+        }
+
+        for (int i = 0; i < N; ++i) {
+            if (!visit[i]) {
+                visit[i] = true;
+                list2[depth] = list[i];
+                backTracking(depth + 1);
+                visit[i] = false;
+            }
+        }
     }
 }
